dc114ef11c6a9891259edb2581db636153663d59
Molik, David <dmolik@cshl.edu>
2015-12-16 14:09:40 -0500

    lets go all the way

--- brb_big_red_button_github.sh
+++ brb_big_red_button_github.sh
@@ -8,7 +8,7 @@ REMOTES_INCOMING="github,master,git@github.com:status-five/BIG-RED-BUTTON.git gi
 REMOTES_OUTGOING="github,master,git@github.com:status-five/BIG-RED-BUTTON.git gitlab,master,git@vcs.cshl.edu:dmolik/BIG-RED-BUTTON.git"
 RELEASE_BRANCH="master"
 LOG="$( which big_red_button.sh | sed "s/$(basename "$( which "big_red_button.sh" )" )//g" )/log/$NAME-$(date "+%Y-%m-%d")"
-FILES_TO_UPDATE="brb_big_red_button.sh"
+FILES_TO_UPDATE="brb_big_red_button_github.sh README.md"
 SPECIAL_INSTRUCTIONS () {
  MSG "Special Instructions" 
 }
